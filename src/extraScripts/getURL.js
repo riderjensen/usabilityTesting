bb736b9540c9e83c0bcbe32f8f4a3eda0f464838
Unknown <riderjensen@gmail.com>
2018-07-10 16:26:04 -0600

    Simple changes on syntax

--- src/extraScripts/getURL.js
+++ src/extraScripts/getURL.js
@@ -2,7 +2,7 @@ const request = require('request');
 const fs = require('fs');
 const { MongoClient } = require('mongodb');
 
-module.exports={
+module.exports = {
     requestURL(URL) {
         // Need to check for any issues in the requesting URL before moving it to the request
         // if there are errors, need to return e
@@ -11,33 +11,32 @@ module.exports={
 
         let chunkNumber = 0;
 
-        s.on('data', function(chunk){
-            chunkNumber++;
-            console.log("CHUNK NUMBER: " + chunkNumber);
-            var chunkString = chunk.toString();
+        s.on('data', (chunk) => {
+            chunkNumber += 1;
+            console.log(`CHUNK NUMBER: ${chunkNumber}`);
+            const chunkString = chunk.toString();
 
-            let changeHREFString = chunkString.replace(/href="http/g, 'HREF="HTTP');
-            let replaceHREFString = changeHREFString.replace(/href="\/|href="/g, 'href="'+requestingURL+'/');
-            let changeHREFBack = replaceHREFString.replace(/HREF="HTTP/g, 'href="http');
-            let changeSRCString = changeHREFBack.replace(/src="http/g, 'SRC="HTTP');
-            let replaceSRCString = changeSRCString.replace(/src="\/|src="/g, 'src="'+requestingURL+'/');
-            let changeSRCBack = replaceSRCString.replace(/SRC="HTTP/g, 'src="http');
-            let changeStyleURL = changeSRCBack.replace(/url\(http/g, 'URL\(HTTP');
-            let replaceStyleURL = changeStyleURL.replace(/url\(/g, 'url('+requestingURL+'/');
-            let changeStyleBack = replaceStyleURL.replace(/URL\(HTTP/g, 'url\(http');
-            
+            const changeHREFString = chunkString.replace(/href="http/g, 'HREF="HTTP');
+            const replaceHREFString = changeHREFString.replace(/href="\/|href="/g, `href="${requestingURL}/`);
+            const changeHREFBack = replaceHREFString.replace(/HREF="HTTP/g, 'href="http');
+            const changeSRCString = changeHREFBack.replace(/src="http/g, 'SRC="HTTP');
+            const replaceSRCString = changeSRCString.replace(/src="\/|src="/g, `src="${requestingURL}/`);
+            const changeSRCBack = replaceSRCString.replace(/SRC="HTTP/g, 'src="http');
+            const changeStyleURL = changeSRCBack.replace(/url\(http/g, 'URL(HTTP');
+            const replaceStyleURL = changeStyleURL.replace(/url\(/g, `url(${requestingURL}/`);
+            const changeStyleBack = replaceStyleURL.replace(/URL\(HTTP/g, 'url(http');
 
-            // This creates a file currently but need to replace with code to push it into mongo BSON file
+            // This creates a file currently but need to replace
+            // with code to push it into mongo BSON file
             // See GridFS in mongo documentation
-            fs.appendFile('index.html', changeStyleBack, function(err) {
+            fs.appendFile('index.html', changeStyleBack, (err) => {
                 if (err) throw err;
-            })
-
+            });
         });
 
-        s.on('end', function() {
+        s.on('end', () => {
             console.log('DONE');
-        })
+        });
     }
-}
+};
 
