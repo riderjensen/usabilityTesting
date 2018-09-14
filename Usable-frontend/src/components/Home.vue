78e6995dac169a42c4066de523df4982da62a7c0
Kenny Stephens <kennethcstephens@gmail.com>
2018-09-14 09:09:01 -0600

    Setting up initial components

---
+++ Usable-frontend/src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <button>Submit Site</button>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ Usable-frontend/src/components/Nav.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
+
