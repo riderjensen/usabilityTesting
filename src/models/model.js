9100ec5450787484ae95b0ee0f9b8c174a7768bc
Rider <riderjensen@gmail.com>
2018-10-08 17:46:24 -0600

    Setting up models more in-depth

--- src/models/model.js
+++ src/models/model.js
@@ -2,16 +2,54 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const websiteStorage = new Schema({
-  webURL: String,
-  testArray: Array,
-  createdAt: { type: Date, default: Date.now }
+  	webURL: String,
+  	testArray: Array,
+  	createdAt: { type: Date, default: Date.now }
 });
 module.exports = mongoose.model("webStorage", websiteStorage);
 
-const userStorage = new Schema({
-  username: String,
-  password: String,
-  projects: Array,
-  createdAt: { type: Date, default: Date.now }
+// userStorage.projects id coresponds to the first page created from the test
+// each userStorage.projects need to be an object that contains the projectID and the questions
+const userStorage = new Schema({	
+	username: String,
+  	password: String,
+  	// array of objects
+  	projects: Array,
+  	createdAt: { type: Date, default: Date.now }
 });
 module.exports = mongoose.model("userStorage", userStorage);
+
+// associated id connects to projects in userStorage.projects array
+const userTracking = new Schema({
+	associatedID: String,
+	// array of objects that we continuously push on to
+	recMoves: Array
+});
+module.exports = mongoose.model("userTracking", userTracking);
+
+
+
+
+
+// this will go in the projects array
+// new object = {
+// 	projectID : String,
+// 	questionArry: {
+// 		// question that needs to be answered
+// 		question: String,
+// 		// shortAnswer, multipleChoice, select
+// 		type: String,
+// 		answers: [
+// 			{
+// 				answer: String,
+// 				correct: Boolean
+// 			},{
+// 				answer: String,
+// 				correct: Boolean
+// 			},{
+// 				answer: String,
+// 				correct: Boolean
+// 			}
+// 		]
+// 	}
+// }
