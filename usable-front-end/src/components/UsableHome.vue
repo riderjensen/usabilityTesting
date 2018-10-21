ef2a24bf0b16657fffeb6ae3c60592398f3b4be2
Kenny Stephens <kennethcstephens@gmail.com>
2018-10-21 10:09:59 -0600

    Correct and updated front end file

---
+++ usable-front-end/src/components/UsableHome.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-layout id="main-section" wrap align-content-center>
+      <v-flex xs12>
+        <h1 class="text-sm-center display-1 font-weight-medium">A testing platform to enhance your <span style="text-decoration: underline;">User's Experience</span></h1>
+      </v-flex>
+
+      <v-flex xs12 class="text-sm-center">
+        <input class="mt-4" type="text" placeholder="Enter Site Link...">
+        <v-btn class="d-block ma-auto mt-3 pl-5 pr-5" round color="cyan" @click="componentChange">CONTINUE<i class="ml-2 fas fa-chevron-right"></i></v-btn>
+        <p class="pt-5">New to <span class=" font-italic font-weight-medium">usable</span>? <span style="text-decoration: underline;" class="learn">Click Here</span> to learn how to use the platform!</p>
+      </v-flex>
+    </v-layout>
+</template>
+
+<script>
+  export default {
+    props: ['selectedComponent'],
+    methods: { 
+        componentChange() {
+        this.selectedComponent = 'UsableForm';
+        this.$emit('changeComponent', this.selectedComponent);
+      }
+    }
+  }
+
+
+</script>
+
+<style scoped>
+  #main-section {
+    height: 100%;
+  }
+
+  input {
+    border: 2px solid white;
+    border-radius: 50px;
+    padding: 10px 20px;
+    width: 50%;
+    background: rgb(14, 18, 20);
+  }
+
+  .learn:hover{
+  color: #00bcd4;
+  cursor: pointer;
+  }
+</style>
---
+++ usable-front-end/src/components/UsableNavbar.vue
@@ -0,0 +1,126 @@
+<template>
+<v-layout align-center>
+  <v-flex xs8>
+    <img src="../assets/usable_logo.svg" alt="">
+  </v-flex>
+
+  <v-flex xs4 class="text-xs-right">
+    <span
+      class="login title font-weight-thin"
+      @click="dialog=true"
+    >LOG IN</span>
+    <span class="ml-2 mr-2 title font-weight-light">|</span>
+    <span class="font-weight-bold title contact">CONTACT US</span>
+    <v-dialog
+      v-model="dialog"
+      width="600"
+    >
+
+      
+
+      <v-card>
+        <v-card-title
+          class="headline grey justify-center"
+          primary-title
+        >
+          Log Into <span><img class="ml-2" src="../assets/usable_logo.svg" alt=""></span>
+        </v-card-title>
+
+        <v-card-text>
+          <input type="text" placeholder="Email Address">
+          <input type="text" placeholder="Password">
+          <v-layout row>
+            <v-flex xs6>
+        <v-checkbox 
+        class="pl-2"
+        :label="`Remember Me`" 
+        v-model="checkbox"></v-checkbox>
+            </v-flex>
+            <v-flex class="d-flex text-xs-right pr-2" xs6 align-center>
+              <span class="forgot">Forgot Password</span>
+            </v-flex>
+          </v-layout>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions class="d-block text-xs-center">
+          <v-spacer></v-spacer>
+          <v-btn
+            style="border-radius: 20px;"
+            class="pr-5 pl-5 mt-3"
+            color="cyan"
+            @click="dialog = false"
+          >
+            Log In
+          </v-btn>
+          <p class="or">or</p>
+          <v-btn
+          style="border-radius: 20px;"
+          class="pr-5 pl-5 mb-3"
+          color="grey"
+          @click="dialog = false"
+          >
+          CREATE NEW ACCOUNT
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+  </v-flex>
+  
+</v-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+      checkbox: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .login, .contact {
+    cursor: pointer;
+  }
+  
+  .login:hover, .contact:hover, .forgot:hover {
+    color: #00bcd4;
+    cursor: pointer;
+  }
+
+  input {
+    border: 1px solid white;
+    border-radius: 20px;
+    color: white;
+    padding: 10px 20px;
+    width: 100%;
+    background: #455A64;
+    margin: 10px 0px;
+  }
+
+  ::placeholder {
+    color: #fff;
+  }
+
+  .or {
+    text-align: center;
+    color: #fff;
+    position: relative;
+    margin: 15px 0;
+  }
+
+  /* .or::before, .or::after {
+    position: absolute; 
+    width: 35%;
+    height: 1px;
+    top: 60%;
+    background-color: #fff;
+    content: '';
+  } */
+</style>
+
