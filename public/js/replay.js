61b3a3ed58cb90dde71b6a1251e498a7a118c74d
Unknown <riderjensen@gmail.com>
2018-10-30 13:26:05 -0600

    Working on fixing array scroll on replay

--- public/js/replay.js
+++ public/js/replay.js
@@ -12,12 +12,18 @@ let scrollOnPage = 0;
 // we need to let the user be able to modify this and change it so that they can replay at their leaisure
 let interval = 100;
 
-
-const result = userMoves.filter((move, i) => {
-	move.ev ? i : false
+// an array of all the positions where .ev is present. Every other occurance of .ev will be a start or a stop and therefore we can get the time between those and tween the correct amount
+let scrollIndexArray = [];
+userMoves.forEach((move, i) => {
+	if (typeof move.ev === "object") {
+		scrollIndexArray.push(i);
+	}
 });
-
+console.log(scrollIndexArray);
+// iterator for userMoves
 let i = 0;
+// iterator for scrolling object
+let j = 0;
 setInterval(function () {
 	TweenLite.to('#box', 1, {
 		ease: Power2.easeNone,
@@ -32,34 +38,28 @@ setInterval(function () {
 		pointer.style.left = userMoves[i].x + '%';
 		pointer.style.top = (userMoves[i].y + scrollOnPage) + '%';
 	}
-	if (userMoves[i].ev) {
-		let ourObj = userMoves[i].ev;
+	if (typeof userMoves[i].ev === "object") {
+		// if the start scroll is here then do tween otherwise if it is just end scroll then dont do anything?
+		let ourTime = scrollIndexArray[j + 1] - scrollIndexArray[j];
+		console.log(ourTime);
+		// this should get the start usermove object
+		// userMoves[scrollIndexArray[j]]
+		// this should get the end usermove object
+		// userMoves[scrollIndexArray[j+1]]
+		let ourObj = userMoves[scrollIndexArray[j + 1]].ev;
 		console.log(ourObj);
-		if (ourObj.type == 'start') {
-			// window.scrollTo(0, ourObj.sScroll)
-			TweenLite.to(window, 1, {
-				scrollTo: {
-					y: ourObj.sScroll,
-					x: 0
-				},
-				ease: Power2.easeInOut
-			});
-		} else if (ourObj.type == 'end') {
-			// window.scrollTo(0, ourObj.eScroll)
-			TweenLite.to(window, 1, {
-				scrollTo: {
-					y: ourObj.eScroll,
-					x: 0
-				},
-				ease: Power2.easeInOut
-			});
-		} else {
-			console.log('missing scroll event');
-		}
+		TweenLite.to(window, ourTime, {
+			scrollTo: {
+				y: ourObj.eScroll,
+				x: 0
+			},
+			ease: Power2.easeInOut
+		});
 	}
 	if (i >= (userMoves.length - 1)) {
 		// reset i to repeat the pattern
 		i = -1;
 	}
 	i++;
+
 }, interval);
