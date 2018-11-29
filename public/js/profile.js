0d4e963e07a6b9c45167ecbe0d1a6a4c45e15825
Unknown <riderjensen@gmail.com>
2018-11-29 11:43:49 -0700

    Adding an event listener to every test

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,1 +1,14 @@
 // probably use AJAX to check the ID they enter against the database to see if there is a test with that id
+
+let classname = document.getElementsByClassName("spanCheking");
+
+
+Array.from(classname).forEach((element) => {
+	element.addEventListener('click', () => {
+		console.log(element.childNodes);
+		document.getElementById('deleteTestID').value = element.childNodes[1].id;
+		document.getElementById('copyTestID').value = element.childNodes[1].id;
+		document.getElementById('deleteTestIDPrompt').innerHTML = `Do you really want to delete ${element.childNodes[1].innerText}?`
+		document.getElementById('copyTestIDPrompt').innerHTML = `Do you really want to copy ${element.childNodes[1].innerText}?`
+	});
+});
