fea7ed6da844ae7fa3fce65d4bfb08869f8309f0
Unknown <riderjensen@gmail.com>
2018-11-29 11:46:56 -0700

    Minor syntax change

--- public/js/profile.js
+++ public/js/profile.js
@@ -5,10 +5,9 @@ let classname = document.getElementsByClassName("spanCheking");
 
 Array.from(classname).forEach((element) => {
 	element.addEventListener('click', () => {
-		console.log(element.childNodes);
 		document.getElementById('deleteTestID').value = element.childNodes[1].id;
 		document.getElementById('copyTestID').value = element.childNodes[1].id;
-		document.getElementById('deleteTestIDPrompt').innerHTML = `Do you really want to delete ${element.childNodes[1].innerText}?`
-		document.getElementById('copyTestIDPrompt').innerHTML = `Do you really want to copy ${element.childNodes[1].innerText}?`
+		document.getElementById('deleteTestIDPrompt').innerHTML = `Do you really want to delete ${element.childNodes[1].innerText}?`;
+		document.getElementById('copyTestIDPrompt').innerHTML = `Do you really want to copy ${element.childNodes[1].innerText}?`;
 	});
 });
