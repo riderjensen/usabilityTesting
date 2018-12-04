75cb536c0e1a2ab56b4d487cee3eb813c60bfac4
Unknown <riderjensen@gmail.com>
2018-12-04 15:48:53 -0700

    Fixing error when id = -1, still need to figure out where that is happen

--- src/routes/replayRouter.js
+++ src/routes/replayRouter.js
@@ -19,37 +19,42 @@ function router() {
 	replayRouter.route('/first/:id')
 		.get((req, res) => {
 			const reqID = req.params.id;
-			(async function mongo() {
-				try {
-					let db = mongoUtil.getDb();
-					let webCol = db.collection('websites');
-					const websiteFind = await webCol.findOne({
-						"testArray": ObjectId(reqID)
-					})
-					const ourWeb = websiteFind;
-					(async function anotherMongo() {
-						try {
-							const ourUpperTestId = ObjectId(ourWeb._id);
-							let col = db.collection('userTracking');
-							const findTracking = await col.findOne({
-								"_id": ObjectId(reqID)
-							});
-							let firstPageID = ourWeb._id;
-							const initInfo = findTracking.initInformation;
-							res.render(`files/${ourUpperTestId}`, {
-								initInfo,
-								reqID,
-								firstPageID
-							});
-						} catch (err) {
-							console.log(err);
-						}
+			if (reqID == -1) {
+				res.send(`It seems that the there is an issue with this test ID; The replay was not saved and an ID was not assigned.`);
+			} else {
+				(async function mongo() {
+					try {
+						let db = mongoUtil.getDb();
+						let webCol = db.collection('websites');
+						const websiteFind = await webCol.findOne({
+							"testArray": ObjectId(reqID)
+						})
+						const ourWeb = websiteFind;
+						(async function anotherMongo() {
+							try {
+								const ourUpperTestId = ObjectId(ourWeb._id);
+								let col = db.collection('userTracking');
+								const findTracking = await col.findOne({
+									"_id": ObjectId(reqID)
+								});
+								let firstPageID = ourWeb._id;
+								const initInfo = findTracking.initInformation;
+								res.render(`files/${ourUpperTestId}`, {
+									initInfo,
+									reqID,
+									firstPageID
+								});
+							} catch (err) {
+								console.log(err);
+							}
 
-					}());
-				} catch (err) {
-					console.log(err.stack);
-				}
-			}());
+						}());
+					} catch (err) {
+						console.log(err.stack);
+					}
+				}());
+			}
+
 		});
 	replayRouter.route('/:id')
 		.get((req, res) => {
