592e484389ca4f92b71b8cc1f4226564d75a99af
Unknown <riderjensen@gmail.com>
2018-12-06 10:03:49 -0700

    Script file creation

---
+++ public/js/completedTest.js
@@ -0,0 +1,24 @@
+// get the cookies stored
+function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return false;
+}
+
+console.log(getCookie('feedbackArray'));
+console.log(getCookie('userTestingArray'));
+
+// display them on the page
+
+
+// have them post to the next page
