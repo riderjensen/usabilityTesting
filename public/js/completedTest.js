1f001d6e592a4c33e6dc3dadd43f821326c82424
Unknown <riderjensen@gmail.com>
2018-12-06 10:12:17 -0700

    Adding form creation elements

--- public/js/completedTest.js
+++ public/js/completedTest.js
@@ -17,8 +17,23 @@ function getCookie(cname) {
 
 console.log(getCookie('feedbackArray'));
 console.log(getCookie('userTestingArray'));
+let userQuestionsArray = JSON.parse(getCookie('userTestingArray'));
+let userFeedbackArray = JSON.parse(getCookie('feedbackArray'));
 
 // display them on the page
 
+let ourPageForm = document.getElementById('completedForm');
+for (let i = 0; i < userQuestionsArray.length; i++) {
+	let label = createElement('label');
+	label.innerText = userQuestionsArray[i];
+	let input = createElement('input');
+	input.setAttribute('class', 'form-control');
+	input.setAttribute('name', 'userResp' + i);
+	input.setAttribute('value', userFeedbackArray[i]);
+	label.appendChild(input);
+	ourPageForm.appendChild(label);
+}
 
-// have them post to the next page
+function createElement(elemName) {
+	return document.createElement(elemName);
+}
