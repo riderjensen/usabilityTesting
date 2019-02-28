34ffa887fe6fb6215c68434fdcb3794da882fd29
Rider <riderjensen@gmail.com>
2019-02-28 11:19:27 -0700

    Refactor with user model

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -3,7 +3,7 @@ const {
 	Strategy
 } = require('passport-local');
 const bcrypt = require('bcrypt-nodejs');
-const mongoUtil = require('../../extraScripts/dbConnect');
+const UserModel = require('../../models/userStorage.model');
 
 // this is for cookies
 module.exports = function localStrategy() {
@@ -11,32 +11,22 @@ module.exports = function localStrategy() {
 		usernameField: 'username',
 		passwordField: 'password'
 	}, (username, password, done) => {
-		(async function mongo() {
-			try {
-				let db = mongoUtil.getDb();
-				const col = db.collection('users');
-				const user = await col.findOne({
-					username
+		UserModel.findOne({ username: username }).then(resp => {
+			if (resp === null) {
+				// no user
+				done(null, false);
+			} else if (resp != null) {
+				bcrypt.compare(password, resp.password, (err, res) => {
+					if (res === true) {
+						done(null, resp);
+					} else {
+						// Return something that says bad pass
+						done(null, false);
+					}
 				});
-
-				if (user === null) {
-					// no user
-					done(null, false);
-				} else if (user != null) {
-					bcrypt.compare(password, user.password, (err, res) => {
-						if (res === true) {
-							done(null, user);
-						} else {
-							// Return something that says bad pass
-							done(null, false);
-						}
-					});
-				} else {
-					done(null, false);
-				}
-			} catch (err) {
-				console.log(err.stack);
+			} else {
+				done(null, false);
 			}
-		}());
+		}).catch(err => console.log(err.stack))
 	}));
 };
