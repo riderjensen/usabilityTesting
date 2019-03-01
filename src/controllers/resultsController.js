33588a0f6cab8536c22c6e8460e1fdf099436add
Rider <riderjensen@gmail.com>
2019-02-28 17:26:51 -0700

    Return page after all calls are returned

--- src/controllers/resultsController.js
+++ src/controllers/resultsController.js
@@ -18,10 +18,8 @@ exports.displayTheResults = (req, res) => {
 
 	WebsiteModel.findById(reqID).then(testFound => {
 		if (testFound == null || testFound.testArray == null) {
-			res.render('404', {
-				errMsg: 'Your results page cannot be found. This is usually a cookie error on your browser. Try deleteing all your cookies and returning to this page.',
-				user: req.user
-			});
+			throw 'Test array is null, we cant find the test'
+
 		} else {
 			testArray = testFound.testArray;
 			questionArray = testFound.questionArray;
@@ -29,40 +27,37 @@ exports.displayTheResults = (req, res) => {
 			createdDate = testFound.createdAt;
 			testName = testFound.testName;
 			url = testFound.shortURL;
-		}
-	}).then(testArray => {
-		if (testArray == null) {
-			throw 'Test array is null, we cant find the test'
-		} else {
+			let ajaxCalls = testArray.length;
 			for (let i = 0; i < testArray.length; i++) {
-
 				UserTrackingModel.findById(testArray[i]).then(testFound => {
 					if (testFound != null) {
 						ourUserInfoArray.push(testFound.userData);
 						ourUserStatesArray.push(testFound.initInformation);
 						ourUserAnswers.push(testFound.finalAnswers)
 					}
-				}).then(() => {
-					res.render('resultsPage', {
-						ourUserInfoArray,
-						ourUserStatesArray,
-						questionArray,
-						webURL,
-						createdDate,
-						testName,
-						reqID,
-						usableURL,
-						url,
-						ourUserAnswers,
-						user: req.user
-					});
+					--ajaxCalls;
+					if (ajaxCalls <= 0) {
+						res.render('resultsPage', {
+							ourUserInfoArray,
+							ourUserStatesArray,
+							questionArray,
+							webURL,
+							createdDate,
+							testName,
+							reqID,
+							usableURL,
+							url,
+							ourUserAnswers,
+							user: req.user
+						});
+					}
 				})
-
 			}
 		}
+
 	}).catch(err => {
 		res.render('404', {
-			errMsg: `Your results page cannot be found. Please refer to the error message:${err.stack}`,
+			errMsg: `Your results page cannot be found. Please refer to the error message:${err}`,
 			user: req.user
 		})
 	})
